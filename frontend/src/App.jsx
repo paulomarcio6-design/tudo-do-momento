da183e89be7abcfc45ce35f60009481b2bdd9975
paulomarcio6-design <paulomarcio6@gmail.com>
2026-03-06 21:13:11 -0300

    Atualização

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { useState, useEffect } from 'react'
-import './App.css'
 
 // Pages
 import HomePage from './pages/HomePage'
